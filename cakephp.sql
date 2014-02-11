cda89c452785f638db3f49b070f837eed41b680e
Shaya Krauss <shaya4207@gmail.com>
2014-02-10 19:15:26 -0500

    initial upload

---
+++ cakephp.sql
@@ -0,0 +1,33 @@
+/*
+SQLyog Professional v11.33 (64 bit)
+MySQL - 5.1.50-community-log : Database - cakephp
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `posts` */
+
+DROP TABLE IF EXISTS `posts`;
+
+CREATE TABLE `posts` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) DEFAULT NULL,
+  `body` text,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `posts` */
+
+insert  into `posts`(`id`,`title`,`body`,`created`,`modified`) values (1,'The title','This is the post body.','2014-02-10 17:25:39',NULL),(2,'A title once again','And the post body follows.','2014-02-10 17:25:39',NULL),(3,'Title strikes back','This is really exciting! Not.','2014-02-10 17:25:39',NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
